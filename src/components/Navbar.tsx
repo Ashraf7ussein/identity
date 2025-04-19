f042612a9d3d6c5800dd7694cf282b745fc54081
ashraf7ussein <ashrafhussein1997@gmail.com>
2025-04-19 11:32:27 -0700

    remove dark

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -11,11 +11,11 @@ const Navbar = () => {
   const [menuOpen, setMenuOpen] = useState(false);
 
   return (
-    <nav className="fixed top-0 left-0 right-0 z-50 border-gray-200 dark:bg-gray-900 bg-[#a2ca57]">
+    <nav className="fixed top-0 left-0 right-0 z-50 border-gray-200 bg-[#a2ca57]">
       <div className="max-w-screen-xl mx-auto flex flex-wrap items-center justify-between p-4">
         <a href="#" className="flex items-center space-x-3 rtl:space-x-reverse">
           <img src={logo} className="w-8" />
-          <span className="self-center text-2xl whitespace-nowrap dark:text-white">
+          <span className="self-center text-2xl whitespace-nowrap">
             Identity
           </span>
         </a>
@@ -24,7 +24,7 @@ const Navbar = () => {
           onClick={() => setMenuOpen(!menuOpen)}
           className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm
          rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2
-        focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+        focus:ring-gray-200"
         >
           <svg
             className="w-5 h-5"
@@ -51,16 +51,14 @@ const Navbar = () => {
           <ul
             className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg 
         bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 
-        md:bg-transparent dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700"
+        md:bg-transparent"
           >
             {navLinks.map((link) => (
               <li key={link.name}>
                 <a
                   href={link.href}
                   className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-[#a2ca57]
-                md:hover:bg-transparent md:border-0 md:hover:text-white md:p-0 
-                dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 
-                dark:hover:text-white md:dark:hover:bg-transparent transition-colors duration-300"
+                md:hover:bg-transparent md:border-0 md:hover:text-white md:p-0 transition-colors duration-300"
                 >
                   {link.name}
                 </a>
